d0949a76bc998e24f5fb8535502268477337254a
aGro1003 <andre.groschopp@meidel-gruppe.com>
2021-04-03 22:24:41 +0200

    Projektdateien hinzufügen.

---
+++ Auto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klassendefinieren_04_2021
+{
+    public class Auto
+    {
+        public string marke;
+        public int alterinJahren;
+
+        public void Beschleunigenauf100kmh()
+        {
+            if (IstSchnell())
+            {
+                Console.WriteLine("Ich habe schnell beschleunigt! Yeah!");
+            }
+            else
+            {
+                Console.WriteLine("Ich habe normal beschleunigt!");
+            }
+            
+        }
+
+        private bool IstSchnell()
+        {
+            if (marke == "Porsche")
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
